45c33fc4fd0b94779c7f9242afda6ab9698cde51
EAxMAC <macos@MacOss-iMac.local>
2020-10-20 23:10:23 +0300

    Finalized Preload

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { AppLoading } from "expo";
-import React, { useState } from "react";
-import { StyleSheet, View, Image } from "react-native";
+import React from "react";
+import { StyleSheet, View } from "react-native";
 import RestaurantsScreen from "./screens/RestaurantsScreen";
 import ProfileScreen from "./screens/ProfileScreen";
 import TestingScreen from "./screens/TestingScreen";
@@ -9,14 +9,13 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import RestaurantLogo from "./logos/RestaurantLogo";
 import ProfileLogo from "./logos/ProfileLogo";
-import { get } from "react-native/Libraries/TurboModule/TurboModuleRegistry";
 
 // all the init happens here
 // later make it a logo display with bg tasks happening
 // db init here
 // opens  other screens where all the stuff happens
 
-async function getFonts(){
+async function preload(){
   await Font.loadAsync({
     bold: require("./assets/fonts/Montserrat-Bold.ttf"),
     italic: require("./assets/fonts/Montserrat-Italic.ttf"),
@@ -28,47 +27,61 @@ async function getFonts(){
 
 const Tab = createBottomTabNavigator();
 
-export default function App() {
-  const [fontsLoaded, setFontsLoaded] = useState(false);
-
-  if (!fontsLoaded) {
-    getFonts().then(setFontsLoaded(true))
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      fontsLoaded: false
+     };
   }
-
-  return (
-    <View style={styles.container}>
-      <NavigationContainer>
-        <Tab.Navigator
-          screenOptions={({ route }) => ({
-            tabBarIcon: ({ focused, color, size }) => {
-              switch (route.name) {
-                case "RestaurantsScreen":
-                  return <RestaurantLogo color={color} />;
-                case "ProfileScreen":
-                  return <ProfileLogo color={color} />;
-                case "TestingScreen":
-                  break;
-              }
-            },
-          })}
-          tabBarOptions={{
-            showLabel: false,
-            style: {
-              backgroundColor: "#f9f9f9",
-            },
-          }}
-        >
-          <Tab.Screen
-            name="RestaurantsScreen"
-            component={RestaurantsScreen}
-          />
-          <Tab.Screen name="ProfileScreen" component={ProfileScreen} />
-          <Tab.Screen name="TestingScreen" component={TestingScreen} />
-        </Tab.Navigator>
-      </NavigationContainer>
-    </View>
-  );  
+  render() {
+    if (!this.state.fontsLoaded) {
+      return (
+        <AppLoading
+          startAsync={preload}
+          onFinish={() => this.setState({fontsLoaded: true})}
+        />
+      );
+    }
+  
+    return (
+      <View style={styles.container}>
+        <NavigationContainer>
+          <Tab.Navigator
+            screenOptions={({ route }) => ({
+              tabBarIcon: ({ focused, color, size }) => {
+                switch (route.name) {
+                  case "RestaurantsScreen":
+                    return <RestaurantLogo color={color} />;
+                  case "ProfileScreen":
+                    return <ProfileLogo color={color} />;
+                  case "TestingScreen":
+                    break;
+                }
+              },
+            })}
+            tabBarOptions={{
+              showLabel: false,
+              style: {
+                backgroundColor: "#f9f9f9",
+              },
+            }}
+          >
+            <Tab.Screen
+              name="RestaurantsScreen"
+              component={RestaurantsScreen}
+            />
+            <Tab.Screen name="ProfileScreen" component={ProfileScreen} />
+            <Tab.Screen name="TestingScreen" component={TestingScreen} />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </View>
+    );  
+  }
 }
+
+export default App;
+
 
 const styles = StyleSheet.create({
   container: {

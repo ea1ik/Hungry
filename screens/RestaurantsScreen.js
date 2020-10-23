66838bb129c880ff89661aea8155d9eba6284fd9
ea1ik <ea1ik@icloud.com>
2020-10-23 09:23:47 +0300

    Added Swipe Library

--- screens/RestaurantsScreen.js
+++ screens/RestaurantsScreen.js
@@ -1,17 +1,58 @@
 import React, { Component } from "react";
 import { View, StyleSheet } from "react-native";
+import Swiper from "react-native-deck-swiper";
 import SwipingCard from "../components/SwipingCard";
+
+const data = [
+  {
+    id: 1,
+    uri: require("../assets/media/pic1.jpg"),
+    title: "Picture 1",
+  },
+  {
+    id: 2,
+    uri: require("../assets/media/pic2.jpg"),
+    title: "Picture 2",
+  },
+  {
+    id: 3,
+    uri: require("../assets/media/pic3.jpg"),
+    title: "Picture 3",
+  },
+  {
+    id: 4,
+    uri: require("../assets/media/pic4.jpg"),
+    title: "Picture 4",
+  },
+  {
+    id: 5,
+    uri: require("../assets/media/pic5.jpg"),
+    title: "Picture 5",
+  },
+];
+
+const Card = ({ card }) => (
+  <View>
+    <Image src={card.uri}></Image>
+  </View>
+);
 
 class RestaurantsScreen extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      currentIndex: 0,
+    };
   }
 
   render() {
     return (
       <View style={{ flex: 1 }}>
-        <SwipingCard></SwipingCard>
+        <Swiper
+          cards={data}
+          cardIndex={this.state.currentIndex}
+          renderCard={(card) => <Card card={card} />}
+        />
       </View>
     );
   }

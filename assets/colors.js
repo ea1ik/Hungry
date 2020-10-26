7f0f76a3c240031a5c70601fc73f6c8edee3324e
EAxMAC <macos@MacOss-iMac.local>
2020-10-26 15:34:10 +0200

    Added Colors File

---
+++ assets/colors.js
@@ -0,0 +1,7 @@
+export const COLORS = {
+    primaryRed: '#7B0828',
+    secondaryRed: '#A71D31',
+    primaryYellow: '#F6D0B1',
+    secondaryYellow: '#9C6615',
+    primaryContrast: '#020122'
+  }
